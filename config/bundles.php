c0cf6d42ecb88c13ae6792b6e75dfb6715a21212
Arthaud <arthaudp33@gmail.com>
2024-03-13 22:35:12 +0100

    package: remove fosrest and use basic symfony for the moment

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,54 +3,47 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\View\View;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
-#[Rest\Route(path: 'api/auth')]
-class AuthController extends AbstractFOSRestController
+class AuthController extends AbstractController
 {
-    #[Rest\Post(path: '/register', name: 'api_auth_register')]
-    #[Rest\View(statusCode: Response::HTTP_CREATED, serializerGroups: ['user'])]
-    public function register(): View
+    #[Route('/api/auth/register', name: 'api_auth_register')]
+    public function register(): Response
     {
-        return $this->view([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RegisterController.php',
-        ], Response::HTTP_CREATED);
+        return new Response(json_encode([
+            'message' => 'ok',
+        ], JSON_THROW_ON_ERROR), Response::HTTP_CREATED);
     }
 
-    #[Rest\Post(path: '/login', name: 'api_auth_login')]
-    #[Rest\View(statusCode: Response::HTTP_OK)]
-    public function login(#[CurrentUser] ?User $user): View
+    #[Route('/api/auth/login', name: 'api_auth_login')]
+    public function login(#[CurrentUser] ?User $user): Response
     {
         if ($user === null) {
-            return $this->view([
+            return new Response(json_encode([
                 'message' => 'missing credentials',
-            ], Response::HTTP_BAD_REQUEST);
+            ], JSON_THROW_ON_ERROR), Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->view([
+        return new Response(json_encode([
             'user' => $user->getUserIdentifier(),
-        ], Response::HTTP_OK);
+        ], JSON_THROW_ON_ERROR), Response::HTTP_OK);
     }
 
-    #[Rest\Get(path: '/check', name: 'api_auth_check')]
-    #[Rest\View(statusCode: Response::HTTP_OK)]
-    public function check(#[CurrentUser] ?User $user): View
+    #[Route('/api/auth/check', name: 'api_auth_check')]
+    public function check(#[CurrentUser] ?User $user): Response
     {
         if ($user === null) {
-            throw new BadRequestHttpException(message: 'vas te faire enculer <3');
-            //            return $this->view([
-            //                'message' => 'unauthorized',
-            //            ], Response::HTTP_UNAUTHORIZED);
+            return new Response(json_encode([
+                'message' => 'not logged in',
+            ], JSON_THROW_ON_ERROR), Response::HTTP_UNAUTHORIZED);
         }
 
-        return $this->view([
+        return new Response(json_encode([
             'user' => $user->getUserIdentifier(),
-        ], Response::HTTP_OK);
+        ], JSON_THROW_ON_ERROR), Response::HTTP_OK);
     }
 }
