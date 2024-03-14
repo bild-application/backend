3f34f8e7a2ad76b5ee6a8812e4d21117ebae2d89
UncleDrew <mfall@feelity.fr>
2024-03-14 17:47:19 +0100

    :construction: fos rest bundle

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
