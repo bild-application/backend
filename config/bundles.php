7e33605cb618317fa5a018c757059dedadf595cc
UncleDrew <mfall@feelity.fr>
2024-03-15 10:08:25 +0100

    :construction: fos rest bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
