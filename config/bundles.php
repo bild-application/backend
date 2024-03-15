b091a292c64bad665350e03dafc8659ebcaaa678
arthaud-proust <arthaudp33@gmail.com>
2024-03-15 20:14:55 +0100

    package: fix test data persistence

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
