52320b197ccdd99462f9c4f7338cb57947d2f9b8
arthaud-proust <arthaudp33@gmail.com>
2024-03-15 13:58:08 +0100

    refactor: error enum is now an enum

--- src/Enum/ErrorEnum.php
+++ src/Enum/ErrorEnum.php
@@ -4,36 +4,36 @@ declare(strict_types=1);
 
 namespace App\Enum;
 
-abstract class ErrorEnum
+enum ErrorEnum: string
 {
-    public const string ERROR_PASSWORD = 'password_invalid';
-    public const string ERROR_PASSWORD_NOT_MATCH = 'password_not_match';
+    case ERROR_PASSWORD = 'password_invalid';
+    case ERROR_PASSWORD_NOT_MATCH = 'password_not_match';
 
-    public const string CONSTRAINT_LENGTH = 'constraint_length';
-    public const string CONSTRAINT_LENGTH_EXACT = 'constraint_length_exact';
-    public const string CONSTRAINT_NOT_NULL = 'constraint_not_null';
-    public const string CONSTRAINT_NOT_BLANK = 'constraint_not_blank';
-    public const string CONSTRAINT_INVALID_EMAIL = 'constraint_invalid_email';
-    public const string CONSTRAINT_REGEX = 'constraint_regex';
-    public const string CONSTRAINT_UNIQUE = 'constraint_unique';
-    public const string CONSTRAINT_RANGE_MIN = 'constraint_range_min';
-    public const string CONSTRAINT_RANGE_MAX = 'constraint_range_max';
-    public const string CONSTRAINT_NOT_IN_RANGE = 'constraint_not_in_range';
-    public const string CONSTRAINT_INVALID_PHONE = 'constraint_invalid_phone';
+    case CONSTRAINT_LENGTH = 'constraint_length';
+    case CONSTRAINT_LENGTH_EXACT = 'constraint_length_exact';
+    case CONSTRAINT_NOT_NULL = 'constraint_not_null';
+    case CONSTRAINT_NOT_BLANK = 'constraint_not_blank';
+    case CONSTRAINT_INVALID_EMAIL = 'constraint_invalid_email';
+    case CONSTRAINT_REGEX = 'constraint_regex';
+    case CONSTRAINT_UNIQUE = 'constraint_unique';
+    case CONSTRAINT_RANGE_MIN = 'constraint_range_min';
+    case CONSTRAINT_RANGE_MAX = 'constraint_range_max';
+    case CONSTRAINT_NOT_IN_RANGE = 'constraint_not_in_range';
+    case CONSTRAINT_INVALID_PHONE = 'constraint_invalid_phone';
 
-    public const string CODE_INVALID = 'code_invalid';
+    case CODE_INVALID = 'code_invalid';
 
-    public const string FORM_KEY_INVALID = 'form_key_invalid';
-    public const string REQUEST_PASSWORD_ALREADY_EXIST = 'request_password_already_exist';
-    public const string INVALID_CHOICE = 'invalid_choice';
+    case FORM_KEY_INVALID = 'form_key_invalid';
+    case REQUEST_PASSWORD_ALREADY_EXIST = 'request_password_already_exist';
+    case INVALID_CHOICE = 'invalid_choice';
 
-    public const string SYMPTOMS_INVALID = 'symptoms_invalid';
+    case SYMPTOMS_INVALID = 'symptoms_invalid';
 
-    public const string MEDIA_TYPE_INVALID = 'media_type_invalid';
+    case MEDIA_TYPE_INVALID = 'media_type_invalid';
 
-    public const string DATE_INVALID = 'date_invalid';
+    case DATE_INVALID = 'date_invalid';
 
-    public const string SURVEY_ALREADY_DONE = 'survey_already_done';
-    public const string SURVEY_NOT_COMPLETED = 'survey_not_completed';
-    public const string SURVEY_JSON_INVALID = 'survey_json_invalid';
+    case SURVEY_ALREADY_DONE = 'survey_already_done';
+    case SURVEY_NOT_COMPLETED = 'survey_not_completed';
+    case SURVEY_JSON_INVALID = 'survey_json_invalid';
 }
