eb556de3eee11d04d5663dc953c7386289ce3e2b
Arthaud <arthaudp33@gmail.com>
2024-03-23 19:02:33 +0100

    feat: add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\ContentFactory;
+use App\Factory\ProfileFactory;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $aliceAndBobParents = UserFactory::createOne([
+            'email' => 'user@email.com',
+            'password' => 'password',
+        ]);
+
+        ContentFactory::createMany(10, [
+            'user' => $aliceAndBobParents,
+        ]);
+
+        $aliceProfile = ProfileFactory::createOne([
+            'name' => 'Alice',
+            'user' => $aliceAndBobParents,
+        ]);
+
+        ContentFactory::createMany(3, [
+            'user' => $aliceAndBobParents,
+            'profile' => $aliceProfile,
+        ]);
+
+        $bobProfile = ProfileFactory::createOne([
+            'user' => $aliceAndBobParents,
+        ]);
+
+        ContentFactory::createMany(2, [
+            'user' => $aliceAndBobParents,
+            'profile' => $bobProfile,
+        ]);
+    }
+}
